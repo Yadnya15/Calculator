9b77230791f8cbd307dfd6ed845542045dce6da7
yadnya bodhak <yadnyab15@gmail.com>
2026-08-21 15:53:28 +0530

    Implement MainActivity with WebView for calculator

---
+++ app/src/main/java/com/yadnya15/calculator/MainActivity.java
@@ -0,0 +1,27 @@
+package com.yadnya15.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://yadnya15.github.io/Calculator/");
+
+        setContentView(webView);
+    }
+}
